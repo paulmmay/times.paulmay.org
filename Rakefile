91441011bf68bcf3bb8e6d2fdf5a361f77db1809
Paul May <git@paulmay.org>
2013-01-05 12:04:21 -0500

    cleaning out routes, updating Rakefile

--- Rakefile
+++ Rakefile
@@ -1,2 +1,10 @@
 require './index'
 require 'sinatra/activerecord/rake'
+require './app/helpers.rb'
+
+namespace :scheduled do
+desc "Automatically update DB"
+   task :autoupdate do
+   autoUpdate
+ end
+end
--- app/routes.rb
+++ app/routes.rb
@@ -1,18 +1,3 @@
-get '/' do
-  erb :front
-end
-
-get '/update' do
-autoUpdate
-end
-
-# Use Sass with `views/style.scss` as your stylesheet
-# To use, delete `public/stylehseets/style.css`
-get '/css/style.css' do
-  scss :style
-end
-
-# 404 Error
 not_found do
   erb :notfound
 end
