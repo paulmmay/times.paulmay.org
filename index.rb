11607dc2ffbf9ffbb3e5a15026ad058d4b68603d
Paul May <git@paulmay.org>
2013-01-05 12:37:50 -0500

    adding postgres/activerecord connection - not sure if this sis required

--- index.rb
+++ index.rb
@@ -2,23 +2,29 @@ require 'bundler'
 require 'open-uri'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :port     => db.port,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
-)
-
 configure do |c|
   # enable :sessions
   set :root, File.dirname(__FILE__)
   set :views, Proc.new{ File.join(root, "app", "views")}
   set :scss, :style => :compact
-  set :database, ENV['DATABASE_URL'] || "sqlite3://db/development.sqlite"
   ActiveRecord::Base.include_root_in_json = false
+end
+
+configure :development, :test do
+set :database, 'sqlite://development.db'
+end
+
+configure :production do
+  # Database connection
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
 end
 
 require './app/models'
