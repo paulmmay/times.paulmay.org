7ee490ad78926a8f58063a57f64319b033ac0787
Paul May <git@paulmay.org>
2013-03-16 19:20:12 -0400

    adapting Dan Catt's Guardian Picture Grid javascript and css https://github.com/revdancatt/CAT540-guardian-picture-grid

--- public/js/control.js
+++ public/js/control.js
@@ -1,8 +1,33 @@
-windowResized: function() {
-/*
-A modified version of a script by the very talented and hardworking Dan Catt:
-https://github.com/revdancatt/CAT540-guardian-picture-grid/blob/master/js/control.js
-*/
+control = {
+    
+    imgWidth: 460,
+    imgHeight: 276,
+    fontsize: '1em',
+    pageY: 100,
+
+    init: function() {
+
+        //  Do calculations if the window gets resized
+        $(window).bind('resize', function() { utils.windowResized(); });
+
+        //  call the resize function in a secon after page load, juat 'cause
+        setTimeout("utils.windowResized()", 1000);
+
+    }
+};
+
+
+utils = {
+    
+    log: function(msg) {
+        try {
+            console.log(msg);
+        } catch(er) {
+        }
+    },
+
+    windowResized: function() {
+
         //  work out the image sizes
         var fit = parseInt($('body').innerWidth()/440, 10)+1;
         control.imgWidth = parseInt($('body').innerWidth()/fit, 10);
@@ -11,8 +36,11 @@ https://github.com/revdancatt/CAT540-guardian-picture-grid/blob/master/js/contro
         //  work out the font size
         //  at 440px wide the biggest font size should be 1em and line-height 1.2em
         //  220px = 0.5em
-        //control.fontsize = ((control.imgWidth - 220)/220*0.5)+0.5;
+        control.fontsize = ((control.imgWidth - 220)/220*0.5)+0.5;
 
         //  Now set them all up
-        $('div.single_story').css({'width': control.imgWidth, 'height': control.imgHeight});
-}
+        $('div.single_story').css({'width': control.imgWidth, 'height': control.imgHeight, 'font-size': control.fontsize + 'em'});
+
+    }
+
+};
