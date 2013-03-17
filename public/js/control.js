5ee6db11c108dbfea9fb4260d67fdda5b6cac9f6
Paul May <git@paulmay.org>
2013-03-16 21:27:07 -0400

    modifying image sizes

--- public/js/control.js
+++ public/js/control.js
@@ -3,8 +3,8 @@ Image layout control. Adapted from https://github.com/revdancatt/CAT540-guardian
 */
 control = {
     
-    imgWidth: 400,
-    imgHeight: 214,
+    imgWidth: 440,
+    imgHeight: 235,
     fontsize: '1em',
     pageY: 100,
 
@@ -28,11 +28,11 @@ utils = {
 
     windowResized: function() {
         //  work out the image sizes
-        var fit = parseInt($('body').innerWidth()/400, 10)+1;
+        var fit = parseInt($('body').innerWidth()/440, 10)+1;
         control.imgWidth = parseInt($('body').innerWidth()/fit, 10);
-        control.imgHeight = parseInt(214 * control.imgWidth/400, 10);
+        control.imgHeight = parseInt(235 * control.imgWidth/440, 10);
         //  work out the font size
-        //  at 400px wide the biggest font size should be 1em and line-height 1.2em
+        //  at 440px wide the biggest font size should be 1em and line-height 1.2em
         //  220px = 0.5em
         control.fontsize = ((control.imgWidth - 220)/220*0.5)+0.5;
         //  Now set them all up
