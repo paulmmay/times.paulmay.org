7375b705dd5f05c234a07f54f5e6cf1e9fa971f3
Paul May <git@paulmay.org>
2013-03-17 21:15:52 -0400

    matching more stories now - need to work on matching h3

--- index.rb
+++ index.rb
@@ -68,26 +68,26 @@ end
 def updateImages(_uri)
    #Updatetimestamp.create(:updated=>Time.now,:status=>true)
    puts "updateImages started at #{Time.now}"
-   begin
 	   source_home = getData(_uri)
 	   html_doc = Nokogiri::HTML(source_home)
 	   #-------------------------- Elements --------------------------------#	   
-	   image_url = html_doc.at_css('div.story').at_css("img")["src"]
-	   image_credit = html_doc.at_css('div.story').at_css("img")["title"]
-	   image_url = image_url.sub("box_620_330","box_600") #swap out for a higher res image
-	   image_url = image_url.sub("box_460_245","box_600") #swap out for a higher res image - second case
-	   image_link = html_doc.at_css('div.story').at_css("a")['href']
-	   image_link = image_link.gsub! /\t/, ''
-	   image_caption = html_doc.at_css('div.story').at_css("span.h2").text
-	   #puts image_link
-	   #--------------------------------------------------------------------#	
-	   t = Timesimage.where(:image_link =>image_link).first_or_create(:updated=>Time.now,:image_url=>image_url,:image_caption=>image_caption,:image_link=>image_link,:image_credit=>image_credit)
-	   puts "updateImages finished at #{Time.now}"
-	   true
-   rescue Exception=>e
-      puts e
-      false
-   end
+	   stories = html_doc.css('div.story');
+	   stories.each do |story|
+	   begin
+		   image_url = story.at_css("img")["src"]
+		   image_credit = story.at_css("img")["title"]
+		   image_url = image_url.sub("box_620_330","box_600") #swap out for a higher res image
+		   image_url = image_url.sub("box_460_245","box_600") #swap out for a higher res image - second case
+		   image_url = image_url.sub("box_140","box_600") #swap out for a higher res image - third case
+		   image_url = image_url.sub("box_300","box_600") #swap out for a higher res image - third case
+		   image_link = story.at_css("a")['href']
+		   image_link = image_link.gsub! /\t/, ''
+		   image_caption = story.at_css("span.h2").text
+		   t = Timesimage.where(:image_link =>image_link).first_or_create(:updated=>Time.now,:image_url=>image_url,:image_caption=>image_caption,:image_link=>image_link,:image_credit=>image_credit)
+		rescue Exception=>e
+		puts e
+		end
+	  end
 end
 
 #------------------------------------------------------------------------#
